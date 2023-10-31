a1352c45b1c219f8d41963ce754a97343154182f
shai <shai44553@gmail.com>
2023-10-31 19:14:42 +0200

    some js and validation

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const path = require('path');
 const mongoose = require('mongoose');
 const Camp = require('./models/camp');
 const methodOverride = require('method-override');
+const catchAsync = require('./utils/catchAsync');
+const expressError = require('./utils/expressError');
+const ejsMate = require('ejs-mate');
+const {validateSchema} = require('./validateSchema.js');
 
 mongoose.connect('mongodb://127.0.0.1:27017/yelpCamp')
     .then(() => {
@@ -16,11 +20,24 @@ mongoose.connect('mongodb://127.0.0.1:27017/yelpCamp')
 
 
 const app = express();
+
+app.engine('ejs',ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.urlencoded({extended: true}))
 app.use(methodOverride('_method'))
+
+// middleware function for checking validation
+const validateCamp = (req,res,next) => {
+    const {error} = validateSchema.validate(req.body);
+    if (error) {
+        const msg = error.details.map(elem => elem.message).join(',')
+        throw new expressError(msg, 400)
+    } else {
+        next();
+    }
+}
 
 // just home page
 app.get('/', (req,res) => {
@@ -28,49 +45,67 @@ app.get('/', (req,res) => {
 })
 
 // show all campground
-app.get('/campgrounds', async (req,res) => {
+app.get('/campgrounds', catchAsync(async(req,res) => {
     const camps = await Camp.find({});
-    res.render('campgrounds/index', {camps});
-})
+    res.render('campgrounds/allCamps', {camps});
+}))
 
 // New Campground
  app.get('/campgrounds/new', (req,res) => {
     res.render('campgrounds/new');
 })
-app.post('/campgrounds', async (req,res) => {
-    const c = await new Camp(req.body)
-    c.save();
-    res.redirect('/campgrounds')
-})
+// making new campground  
+app.post('/campgrounds', validateCamp, catchAsync(async (req,res,next) => {
+    
+    //if((!req.body.campground))
+    //     throw new expressError('Invalied Campground Data', 400)
+    const c = new Camp(req.body.campground);
+    await c.save();
+    res.redirect(`/campgrounds/${c._id}`)
+    
+}))
 
 // show details of one campground
-app.get('/campgrounds/:id', async (req,res) => {
+app.get('/campgrounds/:id', catchAsync(async (req,res) => {
     const {id} = req.params;
     const c = await Camp.findById(id);
     res.render('campgrounds/show', {c});
-})
+}))
 
 // edit campground
-app.get('/campgrounds/:id/edit', async (req,res) => {
+app.get('/campgrounds/:id/edit', catchAsync(async (req,res) => {
     const {id} = req.params;
     const c = await Camp.findById(id);
     res.render('campgrounds/edit', {c})
-})
+}))
 
 // update campground
-app.patch('/campgrounds/:id', async (req,res) => {
+app.patch('/campgrounds/:id', validateCamp, catchAsync(async (req,res) => {
     const {id} = req.params;
-    const c = await Camp.findByIdAndUpdate(id, req.body);
+    const c = await Camp.findByIdAndUpdate(id, {...req.body.campground});
     c.save();
     res.redirect(`/campgrounds/${c._id}`)
-})
+}))
 
 // delete campground
-app.delete('/campgrounds/:id', async (req,res) => {
+app.delete('/campgrounds/:id', catchAsync(async (req,res) => {
     const {id} = req.params;
     await Camp.findByIdAndDelete(id);
     res.redirect('/campgrounds');
+}))
+
+// in case no route is fit
+app.all('*', (req,res,next) => {
+    next(new expressError('Page Not Found', 404))
 })
+
+// handeling errors
+app.use( (err,req,res,next) => {
+    const {status=500, message='something went wrong'} = err;
+    if(!err.message) err.message = 'Something went wrong!';
+    res.status(status).render('error',{err})
+})
+
 
 // connecting to DB
 app.listen(3000, (req,res) => {
--- seeds/start.js
+++ seeds/start.js
@@ -19,9 +19,13 @@ const seedDB = async () => {
     await Camp.deleteMany({});
     for(let i=0; i<50; i++) {
         const randNum = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20) +10;
         const camp = new Camp ({
             location: `${cities[randNum].city}, ${cities[randNum].state}`,
-            title:`${sample(descriptors)} ${sample(places)}`
+            title:`${sample(descriptors)} ${sample(places)}`,
+            image: 'http://source.unsplash.com/collection/483251',
+            description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elitEligendi, ea aliquam? Quisquam tempora id officia? Mollitia iste dolor tempore nihil! Id consectetur nesciunt ducimus perspiciatis animi quis vero! Cupiditate, sit!',
+            price: price
         });
         await camp.save();
     }
