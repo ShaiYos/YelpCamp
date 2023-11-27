d017d5189e37a93b1ea42af9c5b253315e1ed346
shai <shai44553@gmail.com>
2023-11-27 22:12:06 +0200

    end

--- app.js
+++ app.js
@@ -15,11 +15,16 @@ const flash = require('connect-flash');
 const passport = require('passport');
 const passportLocal = require('passport-local')
 const User = require('./models/user')
+const mongoSanitize = require('express-mongo-sanitize')
+const helmet = require('helmet')
+// const MongoDBStore  = require('connect-mongo')(session);
+const MongoStore = require('connect-mongo');
 
 const campgroundsRouter = require('./routes/campgrounds');
 const reviewsRouter = require('./routes/reviews');
 const usersRouter = require('./routes/users');
 
+// const dbUrl = process.env.DB_URL;
 mongoose.connect('mongodb://127.0.0.1:27017/yelpCamp')
     .then(() => {
         console.log("Database connected!");
@@ -38,19 +43,82 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({extended: true}))
 app.use(methodOverride('_method'))
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(mongoSanitize());
 
+const store = MongoStore.create({
+    mongoUrl: 'mongodb://127.0.0.1:27017/yelpCamp',
+    touchAfter: 24 * 60 * 60,
+    crypto: {
+        secret: 'thisshouldbeabettersecret!'
+    }
+});
+
+store.on("error", function (e) {
+    console.log("SESSION STORE ERROR",e)
+})
 const sessionConfig = {
+    store,
+    name: 'session',
     secret: 'thissouldbeabettersecret!',
     resave: false,
     saveUninitialized: true,
     cookie: {
         httpOnly: true,
+        //secure: true,
         expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
         maxAge: 1000 * 60 * 60 * 24 * 7
     }
 }
 app.use(session(sessionConfig));
 app.use(flash());
+app.use(helmet());
+
+const scriptSrcUrls = [
+    "https://stackpath.bootstrapcdn.com",
+    "https://api.tiles.mapbox.com",
+    "https://api.mapbox.com",
+    "https://kit.fontawesome.com",
+    "https://cdnjs.cloudflare.com",
+    "https://cdn.jsdelivr.net",
+];
+const styleSrcUrls = [
+    "https://kit-free.fontawesome.com",
+    "https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css",
+    "https://api.mapbox.com",
+    "https://api.tiles.mapbox.com",
+    "https://fonts.googleapis.com",
+    "https://use.fontawesome.com",
+];
+const connectSrcUrls = [
+    "https://api.mapbox.com",
+    "https://*.tiles.mapbox.com",
+    "https://events.mapbox.com",
+];
+const fontSrcUrls = [];
+app.use(
+    helmet.contentSecurityPolicy({
+        directives: {
+            defaultSrc: [],
+            connectSrc: ["'self'", ...connectSrcUrls],
+            scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
+            styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
+            workerSrc: ["'self'", "blob:"],
+            childSrc: ["blob:"],
+            objectSrc: [],
+            imgSrc: [
+                "'self'",
+                "blob:",
+                "data:",
+                "https://res.cloudinary.com/drqf2nzjq/", 
+                "https://images.unsplash.com",
+                "https://wallpaperset.com"
+            ],
+            fontSrc: ["'self'", ...fontSrcUrls],
+        },
+    })
+);
+
+
 
 app.use(passport.initialize());
 app.use(passport.session());
@@ -71,12 +139,12 @@ app.use('/campgrounds', campgroundsRouter);
 app.use('/campgrounds/:id/reviews', reviewsRouter);
 
 
-// just home page
+// home page
 app.get('/', (req,res) => {
     res.render('home');
 })
 
-// in case no route is fit
+// in case no route fits
 app.all('*', (req,res,next) => {
     next(new expressError('Page Not Found', 404))
 })
